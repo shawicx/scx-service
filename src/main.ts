2716db6d2fa52d00ae2c6cf80fff309bd5e6e1e8
shawicx <shawicx@skiff.com>
2024-06-02 09:48:44 +0800

    feat: app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
 import { generateSwaggerDocument } from './swagger-document';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
